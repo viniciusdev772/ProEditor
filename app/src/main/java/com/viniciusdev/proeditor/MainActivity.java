b1754e8d8b1bd08c473db9747ec9eb3faecc86fd
ViniciusDev <viniciusdev37@gmail.com>
2024-07-15 23:59:07 -0300

    e

--- app/src/main/java/com/viniciusdev/proeditor/MainActivity.java
+++ app/src/main/java/com/viniciusdev/proeditor/MainActivity.java
@@ -10,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //do
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

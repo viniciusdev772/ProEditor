4459c8fd2f511c404458eb2110978d246d6b7fa7
ViniciusDev <viniciusdev37@gmail.com>
2024-07-24 22:14:39 -0300

    feat: Implement storage permission handling
    This commit implements storage permission handling in the MainActivity.
    
    It introduces a PermissionManager class to handle permission checks and requests.
    
    The MainActivity now uses an ActivityResultLauncher to handle the result of the permission request.
    If the user grants the necessary permissions, the app proceeds; otherwise, a toast message informs the user about the denied permissions.

--- app/src/main/java/com/viniciusdev/proeditor/MainActivity.java
+++ app/src/main/java/com/viniciusdev/proeditor/MainActivity.java
@@ -1,16 +1,33 @@
 package com.viniciusdev.proeditor;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private PermissionManager permissionManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityResultLauncher<String[]> requestPermissionsLauncher = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            Boolean manageExternalStorageGranted = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                manageExternalStorageGranted = Environment.isExternalStorageManager();
+            }
+
+            if (Boolean.TRUE.equals(manageExternalStorageGranted)) {
+
+            } else {
+                Toast.makeText(this, "Permissões de armazenamento negadas", Toast.LENGTH_SHORT).show();
+            }
+        });
+        PermissionManager permissionManager = new PermissionManager(this, requestPermissionsLauncher);
         permissionManager.checkPermissions();
         setContentView(R.layout.activity_main);
 
